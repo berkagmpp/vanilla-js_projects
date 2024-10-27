106261545462f12d3e7c4a5993a7d092a615ad02
berkagmpp <berkagmpp@gmail.com>
2024-10-27 16:08:51 +1300

    Feat: dad-jokes - using async-await to fetch data from an API
    ref: #91accd5e48bc1911e3e77194bc04a04f0b5b25c3

--- dad-jokes-http-request-fetch/script.js
+++ dad-jokes-http-request-fetch/script.js
@@ -1,7 +1,9 @@
-// https://icanhazdadjoke.com/api
+// using .then() to fetch data from an API
 
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
 
@@ -27,4 +29,30 @@ function generateJoke() {
       jokeEl.innerHTML = data.joke;
       // approach data.joke directly
     });
+}
+
+// using async/await to fetch data from an API
+
+const jokeEl2 = document.getElementById('joke2');
+const jokeBtn2 = document.getElementById('jokeBtn2');
+
+jokeBtn2.addEventListener('click', generateJoke2);
+
+generateJoke2();
+
+async function generateJoke2() {
+  const config = {
+    headers: {
+      // headers: an object that contains the request headers
+      Accept: 'application/json',
+    },
+  };
+
+  const response = await fetch('https://icanhazdadjoke.com/', config);
+  // basically sets what we get back from the fetch to the response variable
+
+  const data = await response.json();
+  // waits for the response to be converted to JSON format
+
+  jokeEl2.innerHTML = data.joke;
 }
