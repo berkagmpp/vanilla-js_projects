94131d8ac0578ac5d563150a4e5bcf7def87e39a
berkagmpp <berkagmpp@gmail.com>
2024-10-30 15:03:30 +1300

    Feat: random choice picker - pick random tag and add highlight js: interval to start and end highlight and unhighlight the tag pick random tag with Math.floot(Math.random() * tags.length) setInterval() and setTimeout()
    ref: #e9327a5c324fa4a0ce918a222a51696d7e8c6363

--- ramdom-choice-picker/script.js
+++ ramdom-choice-picker/script.js
@@ -1,10 +1,18 @@
-const tagELement = document.getElementById('tags');
+const tagElement = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 
 textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value);
+
+  // use enter key to erase the input and select a random tag
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -13,12 +21,53 @@ function createTags(input) {
     .filter((tag) => tag.trim() !== '') // remove empty space
     .map((tag) => tag.trim()); // remove white space
 
-  tagELement.innerHTML = ''; // clear the tag element
+  tagElement.innerHTML = ''; // clear the tag element
 
+  // create tag element
   tags.forEach((tag) => {
     const span = document.createElement('span'); // create span element
     span.classList.add('tag'); // add class tag
     span.innerText = tag; // add text to tag
-    tagELement.appendChild(span); // append tag to tag element
+    tagElement.appendChild(span); // append tag to tag element
   });
+}
+
+function randomSelect() {
+  const times = 30;
+  // interval to highlight and unhighlight the tag (choise)
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    // highlight and unhighlight the tag
+    highlightTag(randomTag);
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  // stop the interval and highlight the selected tag
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+// pick random tag
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+// highlight the tag
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+// unhighlight the tag
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight');
 }
