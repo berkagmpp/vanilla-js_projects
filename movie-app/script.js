9fb2208741c4f71b84f09b87bc401271c5f9bc36
berkagmpp <berkagmpp@gmail.com>
2024-11-01 12:37:07 +1300

    Feat: movie app - show movies js: make movie object from recieved json data by forEach() inside forEach() add classList('div') and put innerHTML using movie object, and send via appendChild add dynamic css class which is determined by vote_average
    ref: #0d4b486fe1ef3ac6741155e2990c48f52ba9508a

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,11 +1,12 @@
 const API_URL =
   'http://api.themoviedb.org/3/discover/movie?sort_by=popularity/desc&api_key=9d44c04249c44e3ef9c4b70600bd09f4&page=1';
-const IMG_PATH = 'http://image.tmbd/org/t/p/w1280';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'http://api.themoviedb.org/3/search/movie?api_key=9d44c04249c44e3ef9c4b70600bd09f4&query="';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 // get initial movies
 getMovies(API_URL);
@@ -16,6 +17,38 @@ async function getMovies(url) {
 
   console.log(data.results);
   // Array(20)...
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `
+        <img src="${[IMG_PATH + poster_path]}" alt="${title}" />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        `;
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'oreange';
+  } else {
+    return 'red';
+  }
 }
 
 form.addEventListener('submit', (e) => {
