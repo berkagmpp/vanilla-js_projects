60f9aca2760aca13a93351063868ea4aa6879dc9
berkagmpp <berkagmpp@gmail.com>
2024-11-05 14:12:57 +1300

    Feat: github profiles - display user profile and error handling js: create HTML elements for display with data from user input catch error
    ref: #f89c15f4f25621da5bc25e0adfdd72f61814a299

--- github-profiles-axios/script.js
+++ github-profiles-axios/script.js
@@ -1,5 +1,6 @@
 const APIUTL = 'https://api.github.com/users/';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -9,10 +10,47 @@ async function getUser(username) {
     //   console.log(res.data);
     // res is object of data property -> can be destructive like below
     const { data } = await axios(APIUTL + username);
-    console.log(data);
+    // console.log(data);
+
+    createUserCard(data);
   } catch (err) {
-    console.log(err);
+    if (err.response.status === 404) {
+      createErrorCard('No profile with this user');
+    }
   }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+    <div class="card">
+      <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar" />
+      </div>
+
+      <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+          <li>${user.followers}<strong>Followers</strong></li>
+          <li>${user.following}<strong>Following</strong></li>
+          <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+      </div>
+    </div>`;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+        <div class="card">
+          <h1>${msg}</h1>
+        </div>
+    `;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e) => {
