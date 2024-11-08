3d18ed5641f1d5532a3fdaa04dd31cdec37f0b85
berkagmpp <berkagmpp@gmail.com>
2024-11-09 10:26:01 +1300

    Feat: auto text effect js: control letter showing speed by slice()

---
+++ auto-text-effect/script.js
@@ -0,0 +1,27 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'You can control the speed down there!';
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, idx);
+
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
+
+speedEl.addEventListener('input', (e) => {
+  speed = 300 / e.target.value;
+
+  if (e.target.value > 5) {
+    e.target.value = 5;
+  }
+});
