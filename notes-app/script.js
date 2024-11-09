3039dec8ecf83090e26632b113f9e16120ad049f
berkagmpp <berkagmpp@gmail.com>
2024-11-10 12:03:21 +1300

    Feat: notes app - add edit, delete, textarea markdown js: marked.parse() from cdnjs -> can use merkdown text
    ref: #13807fed99ab5114e9b2380a78800162ebb69378

--- notes-app/script.js
+++ notes-app/script.js
@@ -16,5 +16,28 @@ function addNewNote(text = '') {
         <textarea class ="${text ? 'hidden' : ''}"></textarea>
   `;
 
+  const editBtn = note.querySelector('.edit');
+  const deleteBtn = note.querySelector('.delete');
+  const main = note.querySelector('.main');
+  const textArea = note.querySelector('textarea');
+
+  textArea.value = text;
+  main.innerHTML = marked.parse(text); // marked.parse() from cdnjs -> makes can use markdown
+
+  deleteBtn.addEventListener('click', () => {
+    note.remove();
+  });
+
+  editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden');
+    textArea.classList.toggle('hidden');
+  });
+
+  // display textarea input
+  textArea.addEventListener('input', (e) => {
+    const { value } = e.target; // e.target means textarea itself -> get value from textarea
+    main.innerHTML = marked.parse(value); // display markdown text
+  });
+
   document.body.appendChild(note);
 }
